6cdf0651ef87dd05dc2cadb9426b403aae3272ea
husan <nhusan.dev@gmail.com>
2023-02-17 18:34:17 -0500

    init project

---
+++ src/main/java/com/dairycoders/classycreams/ClassycreamsApplication.java
@@ -0,0 +1,13 @@
+package com.dairycoders.classycreams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassycreamsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassycreamsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dairycoders/classycreams/ClassycreamsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dairycoders.classycreams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassycreamsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
